4fc463153fa7824acb0cdc520a74a9bf0fa80be5
XDDanielTayupovXD <115371460+XDDanielTayupovXD@users.noreply.github.com>
2023-03-19 12:30:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import game
-import pygame
-import pygame_menu
+import  pygame
+import  pygame_menu
+
 
 pygame.init()
 surface = pygame.display.set_mode((600, 400))
@@ -9,25 +10,25 @@ def set_difficulty(value, difficulty):
     # Do the job here !
     pass
 
-def show_scoreboard():
-    global menu
-    menu.clear()
-    with open('scoreboard.txt', 'r') as file:
-        text = file.read()
-        menu.add.label(text, max_char=-1, font_size = 20)
-
 def start_the_game():
     myGame = game.game()
     myGame.play()
+pass
 
+def scoreboard():
+    file = open("scoreboard.txt", "w")
+    file.write(f'{score_1}\t{score_2}')
 
-menu = pygame_menu.Menu('Welcome', 600, 400,
+menu = pygame_menu.Menu('Welcome', 400, 300,
                        theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.text_input('Name :', default='John Doe')
+menu.add.text_input('Name :', default='MSHK FREDE')
 menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
 menu.add.button('Play', start_the_game)
-menu.add.button('Scoreboard', show_scoreboard)
 menu.add.button('Quit', pygame_menu.events.EXIT)
+menu.add.button('Scoreboard', scoreboard)
 
 menu.mainloop(surface)
+
+myGame = game.game()
+myGame.play()
